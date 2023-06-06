5b7e8c2f8286e55a08d1db296fc0e0b56924cc6f
MilaMiloca <milaafrferreira@gmail.com>
2023-06-06 17:36:50 -0300

    Projeto da Mila

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times: Guarda Municipal de Itajaí',
+              'Contador da Camila',
             ),
             Text(
               '$_counter',
